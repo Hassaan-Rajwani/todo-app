296363e2c3dad752d1f7d19d8dc667c6b37b81e5
Hassaan Rajwani <hassaanqadri258@gmail.com>
2021-10-23 01:51:35 +0500

    Modified

--- index.js
+++ index.js
@@ -1,31 +1,50 @@
-// var arr = []
-
+var arr = []
 function app(){
     var mscreen = document.getElementById("screen").value
     var ol = document.getElementById("ol")
-    
-    // arr.push(mscreen)
-    
-    // document.getElementById("ol").innerText = ""
-    
-    // for(var i = 0; i < arr.length; i++){
+    arr.push(mscreen)
+    genList()
+}
+function genList(){
+    document.getElementById("ol").innerText = ""
+    for(var i = 0; i < arr.length; i++){
         var li = document.createElement("li")
-        var list = document.createTextNode(mscreen)
+        var list = document.createTextNode(arr[i])
         li.appendChild(list)
         ol.appendChild(li)
 
         // create btn 1
         var button1 = document.createElement("button")
-        button1.className = "done"
+        button1.id = "edit"
         li.appendChild(button1)
+        button1.onclick = edit(i)
 
-        button1.onclick = function done(){
-            li.style.textDecoration = "line-through"
-            li.style.opacity = "0.5"
-        }
-
-        // btn 2        
+        // create btn 2        
         var button2 = document.createElement("button")
-        button2.className = "delete"
+        button2.id = "delete"
         li.appendChild(button2)
+        button2.onclick = dlt(i)
+    
+        // create btn 3
+        var button3 = document.createElement("button")
+        button3.id = "done"
+        li.appendChild(button3)
+
+        // remove input
+        document.getElementById("screen").value = ""
     }
+}
+
+function dlt(index){
+    return function(){
+        arr.splice(index,1)
+        genList()
+    }
+}
+
+function edit(i){
+    return function(){
+        (document.getElementById("screen").value = arr[i])
+        arr.splice(i,1)
+    }
+}
